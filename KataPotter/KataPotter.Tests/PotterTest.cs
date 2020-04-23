0410531e9b55a53eacb03151f5146175c3855d30
Boris <anas.essaghir@gmail.com>
2020-04-23 15:41:35 +0200

    Add tests for several books with differents cases

--- KataPotter/KataPotter.Tests/PotterTest.cs
+++ KataPotter/KataPotter.Tests/PotterTest.cs
@@ -86,6 +86,92 @@ namespace KataPotter.Tests
             Check.That(store.Bill()).IsEqualTo(30);
         }
 
+        [Fact]
+        public void Given_a_basket_with_2_same_and_one_different_books_should_return_23_2()
+        {
+            var store = new Store();
+            var firstBook = new Book(BookTitle.First);
+            var secondBook = new Book(BookTitle.Second);
+            store.AddToBasket(firstBook, 2);
+            store.AddToBasket(secondBook, 1);
+            Check.That(store.Bill()).IsEqualTo(23.20);
+        }
 
+        [Fact]
+        public void Given_a_basket_with_several_books_should_return_51_2()
+        {
+            var store = new Store();
+            var firstBook = new Book(BookTitle.First);
+            var secondBook = new Book(BookTitle.Second);
+            var thirdBook = new Book(BookTitle.Third);
+            var fourthBook = new Book(BookTitle.Fourth);
+            var fifthBook = new Book(BookTitle.Fifth);
+            store.AddToBasket(firstBook, 2);
+            store.AddToBasket(secondBook, 2);
+            store.AddToBasket(thirdBook, 2);
+            store.AddToBasket(fourthBook, 1);
+            store.AddToBasket(fifthBook, 1);
+            Check.That(store.Bill()).IsEqualTo(51.20);
+        }
+
+        [Fact]
+        public void Given_a_basket_with_several_books_should_return_30_4()
+        {
+            var store = new Store();
+            var firstBook = new Book(BookTitle.First);
+            var secondBook = new Book(BookTitle.Second);
+            store.AddToBasket(firstBook, 2);
+            store.AddToBasket(secondBook, 2);
+            Check.That(store.Bill()).IsEqualTo(2 * (8 * 2 * 0.95));
+        }
+
+        [Fact]
+        public void Given_a_basket_with_several_books_should_return_40_8()
+        {
+            var store = new Store();
+            var firstBook = new Book(BookTitle.First);
+            var secondBook = new Book(BookTitle.Second);
+            var thirdBook = new Book(BookTitle.Third);
+            var fourthBook = new Book(BookTitle.Fourth);
+            store.AddToBasket(firstBook, 2);
+            store.AddToBasket(secondBook, 1);
+            store.AddToBasket(thirdBook, 2);
+            store.AddToBasket(fourthBook, 1);
+            Check.That(store.Bill()).IsEqualTo((8 * 4 * 0.8) + (8 * 2 * 0.95));
+        }
+
+        [Fact]
+        public void Given_a_basket_with_several_books_should_return_38()
+        {
+            var store = new Store();
+            var firstBook = new Book(BookTitle.First);
+            var secondBook = new Book(BookTitle.Second);
+            var thirdBook = new Book(BookTitle.Third);
+            var fourthBook = new Book(BookTitle.Fourth);
+            var fifthBook = new Book(BookTitle.Fifth);
+            store.AddToBasket(firstBook, 1);
+            store.AddToBasket(secondBook, 2);
+            store.AddToBasket(thirdBook, 1);
+            store.AddToBasket(fourthBook, 1);
+            store.AddToBasket(fifthBook, 1);
+            Check.That(store.Bill()).IsEqualTo(8 + (8 * 5 * 0.75));
+        }
+
+        [Fact]
+        public void Given_a_basket_with_several_books_should_return_152()
+        {
+            var store = new Store();
+            var firstBook = new Book(BookTitle.First);
+            var secondBook = new Book(BookTitle.Second);
+            var thirdBook = new Book(BookTitle.Third);
+            var fourthBook = new Book(BookTitle.Fourth);
+            var fifthBook = new Book(BookTitle.Fifth);
+            store.AddToBasket(firstBook, 5);
+            store.AddToBasket(secondBook, 5);
+            store.AddToBasket(thirdBook, 4);
+            store.AddToBasket(fourthBook, 5);
+            store.AddToBasket(fifthBook, 4);
+            Check.That(store.Bill()).IsEqualTo(152);
+        }
     }
 }
--- KataPotter/KataPotter/Store.cs
+++ KataPotter/KataPotter/Store.cs
@@ -6,56 +6,88 @@ namespace KataPotter
 {
     public class Store
     {
+        private const float ONE_BOOK_DISCOUNT = 1f;
         private const float TWO_BOOKS_DISCOUNT = 0.95f;
         private const float THREE_BOOKS_DISCOUNT = 0.90f;
         private const float FOUR_BOOKS_DISCOUNT = 0.80f;
         private const float FIVE_BOOKS_DISCOUNT = 0.75f;
         private const int BOOK_PRICE = 8;
-        private readonly Dictionary<Book,int> _basket;
+        private readonly Dictionary<BookTitle,int> _basket;
         public Store()
         {
-            _basket = new Dictionary<Book, int>();
+            _basket = new Dictionary<BookTitle, int>();
         }
         public double Bill()
         {
             if (!_basket.Any())
                 return 0;
 
-            if (HaveOnlyOneTypeOfBook())
-                return BOOK_PRICE * _basket.Values.Sum();
+            if (hadSameBook())
+                return BOOK_PRICE * QuantitySum();
 
-            return PriceWithReduction();
+            
+            double finalPrice = 0;
+            return Math.Round(PriceWithReduction(finalPrice),2);
         }
 
-        private double PriceWithReduction()
+        private double PriceWithReduction(double finalPrince)
         {
-            switch (_basket.Count)
+            if (_basket.Count == 0)
+                return finalPrince;
+
+            switch (OptimizeReduction())
             {
+                case 1:
+                    return finalPrince + PriceWithReduction(ApplyReduction(ONE_BOOK_DISCOUNT,1));
                 case 2:
-                    return ApplyReduction(TWO_BOOKS_DISCOUNT);
+                    return finalPrince + PriceWithReduction(ApplyReduction(TWO_BOOKS_DISCOUNT,2));
                 case 3:
-                    return ApplyReduction(THREE_BOOKS_DISCOUNT);
+                    return finalPrince + PriceWithReduction(ApplyReduction(THREE_BOOKS_DISCOUNT,3));
                 case 4:
-                    return ApplyReduction(FOUR_BOOKS_DISCOUNT);
+                    return finalPrince + PriceWithReduction(ApplyReduction(FOUR_BOOKS_DISCOUNT,4));
                 default:
-                    return ApplyReduction(FIVE_BOOKS_DISCOUNT);
+                    return finalPrince + PriceWithReduction(ApplyReduction(FIVE_BOOKS_DISCOUNT,5));
             }
         }
 
-        private double ApplyReduction(float reduction)
+        private double ApplyReduction(float reduction,int count)
         {
-            return Math.Round(_basket.Values.Sum() * BOOK_PRICE * reduction, 2);
+            var countElement = count;
+            double result = count * BOOK_PRICE * reduction;
+            foreach (BookTitle book in Enum.GetValues(typeof(BookTitle)))
+            {
+                if (_basket.ContainsKey(book) && countElement > 0)
+                {
+                    _basket[book]--;
+                    countElement--;
+                    if (_basket[book] == 0)
+                        _basket.Remove(book);
+                }
+
+            }
+
+            return result;
         }
 
-        private bool HaveOnlyOneTypeOfBook() => _basket.Count == 1;
+        private bool hadSameBook() => _basket.Count == 1;
+
+        private int QuantitySum() => _basket.Values.Sum();
 
         public void AddToBasket(Book book, int quantity)
         {
-            if(_basket.ContainsKey(book))
-                _basket[book]+=quantity;
+            if (_basket.ContainsKey(book.Title))
+                _basket[book.Title] += quantity;
             else
-                _basket.Add(book, quantity);
-            
+                _basket.Add(book.Title, quantity);
+
+        }
+
+        private int OptimizeReduction()
+        {
+            if (_basket.Count == 5 && _basket.Values.Count(x => x > 1) >= 3 )
+                return 4;
+            return _basket.Count;
+
         }
     }
 }
